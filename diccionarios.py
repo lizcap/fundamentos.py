c23519818d88cb7acb3ce9604b9f3c303af63a56
lizz palacios <lizpalax223@gmail.com>
2023-06-15 14:40:25 -0500

    10 diccionarios.py

---
+++ diccionarios.py
@@ -0,0 +1,20 @@
+#diccionarios
+#{}
+
+
+team ={
+    "name": "city",
+    "country":"england",
+    "champions":1,
+    "players":['halland','grealinsh']
+
+}
+
+print(type(team))
+print(team)
+print(team["name"])
+print(team["players"])
+team["players"].append("kevin")
+team["name"] ="manchester city"
+team["leage"] ="premiere league"
+print(team)
